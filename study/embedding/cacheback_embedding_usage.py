b6e22805d845a65cbb7ef94d3ef1f75afcab39c9
luobin <luobin@unipus.cn>
2026-04-07 21:52:20 +0800

    feat: cacheback_embedding_usage

---
+++ study/embedding/cacheback_embedding_usage.py
@@ -0,0 +1,62 @@
+import dotenv
+from pathlib import Path
+from langchain_openai import OpenAIEmbeddings
+from langchain_classic.embeddings import CacheBackedEmbeddings
+from langchain_classic.storage import LocalFileStore
+
+import numpy
+
+dotenv.load_dotenv()
+
+
+# 2. 缓存目录
+cache_dir = Path(__file__).resolve().parent / "cache"
+
+# 3. 文件缓存 store
+store = LocalFileStore(str(cache_dir))
+
+# 1.创建文本嵌入模型
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+embeddings_with_cache = CacheBackedEmbeddings.from_bytes_store(
+    embeddings,
+    store,
+    namespace=embeddings.model,
+    query_embedding_cache=True,
+)
+
+# 2.嵌入文本
+query_vector = embeddings_with_cache.embed_query("我叫Robin，我喜欢打篮球")
+
+# print(query_vector)
+# print(len(query_vector)) # 1536
+
+# 3.嵌入文档列表/字符串列表
+documents_vector = embeddings_with_cache.embed_documents(
+    ["我叫Robin，我喜欢打篮球", "这个喜欢打篮球的人叫Robin", "求知若渴，虚心若愚"]
+)
+
+print(len(documents_vector))  # 3
+
+
+# 计算相似度
+def cos_similarity(vec1: list, vec2: list) -> float:
+    """计算传入两个向量的余弦相似度"""
+    # 1. 计算两个向量的点积
+    dot_product = numpy.dot(vec1, vec2)
+
+    # 2. 计算两个向量的长度
+    vec1_length = numpy.linalg.norm(vec1)
+    vec2_length = numpy.linalg.norm(vec2)
+
+    # 3.计算余弦相似度
+    return dot_product / (vec1_length * vec2_length)
+
+
+# 计算余弦相似度
+print(
+    "向量1和向量2的相似度：", cos_similarity(documents_vector[0], documents_vector[1])
+)  # 向量1和向量2的相似度： 0.8646846062415526
+
+print(
+    "向量1和向量3的相似度：", cos_similarity(documents_vector[0], documents_vector[2])
+)  # 向量1和向量3的相似度： 0.0868669237226309
